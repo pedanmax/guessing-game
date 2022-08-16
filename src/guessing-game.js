1d4216d79cd4b5cb20ed78a040b51f05d1f7a9f4
pedanmax <m.pedan2017@gmail.com>
2022-08-16 12:33:23 +0300

    feat:i did this task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        // this.min = min;
+        // this.max = max;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
+        return Math.round((this.min - this.max)/2) + this.max;
 
     }
-
     lower() {
-
+        // this.max mid - 1
+        this.max = (Math.round((this.min - this.max)/2) + this.max) 
     }
 
     greater() {
-
+        // this.min mid + 1
+        this.min = (Math.round((this.min - this.max)/2) + this.max) 
     }
 }
 
